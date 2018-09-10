b9ebac8c1c2e2a521dfbe5634a0fe6240ed80b7d
Bloder <aptgetinstalldanielleme@gmail.com>
2018-09-09 21:59:57 -0300

    Create coroutines gson adapter

--- domain/src/main/java/bloder/com/domain/Api.kt
+++ domain/src/main/java/bloder/com/domain/Api.kt
@@ -1,3 +1,30 @@
 package bloder.com.domain
 
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
+abstract class Api<T> {
+
+    protected fun retrofit(endPoint: String): Retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().serializeNulls().create()))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .client(createClientBuilder())
+            .baseUrl(endPoint)
+            .build()
+
+    abstract fun service() : T
+
+    private fun createClientBuilder(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build()
+    }
+}
