7e74075522ea39d393e0b2d75d407273650a809b
Bloder <aptgetinstalldanielleme@gmail.com>
2018-09-12 22:50:14 -0300

    Fix search fixture repo problem

--- domain/src/main/java/bloder/com/domain/repository/fixture/resources/FixtureSearchRepository.kt
+++ domain/src/main/java/bloder/com/domain/repository/fixture/resources/FixtureSearchRepository.kt
@@ -1,10 +1,12 @@
 package bloder.com.domain.repository.fixture.resources
 
+import bloder.com.domain.payloads.Binder
 import bloder.com.domain.repository.resources.SearchRepository
+import bloder.com.domain.values.Search
 
 class FixtureSearchRepository : SearchRepository {
 
-    override suspend fun search(query: String) {
-
+    override suspend fun search(query: String): Binder<Search, Void> {
+        return Binder(null, null)
     }
 }
