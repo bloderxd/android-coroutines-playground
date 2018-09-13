91387aeeba2562fcf0227db674dc473209e6479d
Bloder <aptgetinstalldanielleme@gmail.com>
2018-09-12 22:48:09 -0300

    Change result of release search repo

--- domain/src/main/java/bloder/com/domain/api/search/SearchServices.kt
+++ domain/src/main/java/bloder/com/domain/api/search/SearchServices.kt
@@ -1,10 +1,11 @@
 package bloder.com.domain.api.search
 
+import bloder.com.domain.payloads.SearchPayload
 import bloder.com.domain.values.Search
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 
 interface SearchServices {
 
-    @GET("") fun search(query: String) : Deferred<Search>
+    @GET("") fun search(query: String) : Deferred<SearchPayload>
 }
